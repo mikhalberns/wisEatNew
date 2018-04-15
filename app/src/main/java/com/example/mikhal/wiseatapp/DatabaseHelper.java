73f942dad89ed952c9b462df457471aa527ab301
gaeaStiller <gaea.stiller@gmail.com>
2018-04-15 23:16:36 +0300

    changing profiles db names

--- app/src/main/java/com/example/mikhal/wiseatapp/DatabaseHelper.java
+++ app/src/main/java/com/example/mikhal/wiseatapp/DatabaseHelper.java
@@ -10,30 +10,29 @@ import org.w3c.dom.Text;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     // profiles table
-    public static final String DATABASE_NAME = "wiseatApp.db";
-    public static final String TABLE_NAME = "profiles_table";
-    public static final String COL_1 = "profileID";
-    public static final String COL_2 = "profileName";
-    public static final String COL_3 = "beef";
-    public static final String COL_4 = "chicken";
-    public static final String COL_5 = "pork";
-    public static final String COL_6 = "fish";
-    public static final String COL_7 = "Insects";
-    public static final String COL_8 = "eggs";
-    public static final String COL_9 = "milk";
-    public static final String COL_10 = "honey";
-    public static final String COL_11 = "gluten";
-    public static final String COL_12 = "lupin";
-    public static final String COL_13 = "sesame";
-    public static final String COL_14 = "algae";
-    public static final String COL_15 = "shellfish";
-    public static final String COL_16 = "soy";
-    public static final String COL_17 = "peanuts";
-    public static final String COL_18 = "sulphite";
-    public static final String COL_19 = "nuts";
-    public static final String COL_20 = "mustard";
-    public static final String COL_21 = "celery";
-    public static final String COL_22 = "corn";
+    public static final String WISEATAPP_DATABASE = "wiseatApp.db";
+    public static final String PROFILES_TABLE = "profiles_table";
+    public static final String PROFILEID = "profileID";
+    public static final String BEEF = "beef";
+    public static final String CHICKEN = "chicken";
+    public static final String PORK = "pork";
+    public static final String FISH = "fish";
+    public static final String INSECTS = "Insects";
+    public static final String EGGS = "eggs";
+    public static final String MILK = "milk";
+    public static final String HONEY = "honey";
+    public static final String GLUTEN = "gluten";
+    public static final String LUPIN = "lupin";
+    public static final String SESAME = "sesame";
+    public static final String ALGAE = "algae";
+    public static final String SHELLFISH = "shellfish";
+    public static final String SOY = "soy";
+    public static final String PEANUTS = "peanuts";
+    public static final String SULPHITE = "sulphite";
+    public static final String NUTS = "nuts";
+    public static final String MUSTARD = "mustard";
+    public static final String CELERY = "celery";
+    public static final String CORN = "corn";
 
     // users table
     public static final String USERS_TABLE = "usersTable";
@@ -41,52 +40,52 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String PROFILE_ID = "profileId";
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 2);
+        super(context, WISEATAPP_DATABASE, null, 2);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + USERS_TABLE + " (userId TEXT, profileId INTEGER)");
-        db.execSQL("create table " + TABLE_NAME +" (profileID INTEGER PRIMARY KEY autoincrement,profileName TEXT,beef INTEGER,chicken INTEGER,pork INTEGER,fish INTEGER,Insects INTEGER,eggs INTEGER,milk INTEGER,honey INTEGER,gluten INTEGER,lupin INTEGER,sesame INTEGER,algae INTEGER,shellfish INTEGER,soy INTEGER,peanuts INTEGER,sulphite INTEGER,nuts INTEGER,mustard INTEGER,celery INTEGER,corn INTEGER)");
+        db.execSQL("create table " + PROFILES_TABLE +" (profileID INTEGER PRIMARY KEY autoincrement,beef INTEGER,chicken INTEGER,pork INTEGER,fish INTEGER,Insects INTEGER,eggs INTEGER,milk INTEGER,honey INTEGER,gluten INTEGER,lupin INTEGER,sesame INTEGER,algae INTEGER,shellfish INTEGER,soy INTEGER,peanuts INTEGER,sulphite INTEGER,nuts INTEGER,mustard INTEGER,celery INTEGER,corn INTEGER)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+PROFILES_TABLE);
         db.execSQL("DROP TABLE IF EXISTS "+USERS_TABLE);
         onCreate(db);
     }
 
-   public boolean insertData(Integer beef,Integer chicken,Integer pork,Integer fish,Integer Insects, Integer eggs,Integer milk,Integer honey,Integer gluten,Integer lupin,Integer sesame, Integer algae, Integer shellfish, Integer soy, Integer peanuts,Integer sulphite,Integer nuts, Integer mustard,Integer celery, Integer corn) {
+    public boolean insertData(Integer beef,Integer chicken,Integer pork,Integer fish,Integer Insects, Integer eggs,Integer milk,Integer honey,Integer gluten,Integer lupin,Integer sesame, Integer algae, Integer shellfish, Integer soy, Integer peanuts,Integer sulphite,Integer nuts, Integer mustard,Integer celery, Integer corn) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_3,beef);
-        contentValues.put(COL_4,chicken);
-        contentValues.put(COL_5,pork);
-       contentValues.put(COL_6,fish);
-       contentValues.put(COL_7,Insects);
-       contentValues.put(COL_8,eggs);
-       contentValues.put(COL_9,milk);
-       contentValues.put(COL_10,honey);
-       contentValues.put(COL_11,gluten);
-       contentValues.put(COL_12,lupin);
-       contentValues.put(COL_13,sesame);
-       contentValues.put(COL_14,algae);
-       contentValues.put(COL_15,shellfish);
-       contentValues.put(COL_16,soy);
-       contentValues.put(COL_17,peanuts);
-       contentValues.put(COL_18,sulphite);
-       contentValues.put(COL_19,nuts);
-       contentValues.put(COL_20,mustard);
-       contentValues.put(COL_21,celery);
-       contentValues.put(COL_22,corn);
+        contentValues.put(BEEF,beef);
+        contentValues.put(CHICKEN,chicken);
+        contentValues.put(PORK,pork);
+        contentValues.put(FISH,fish);
+        contentValues.put(INSECTS,Insects);
+        contentValues.put(EGGS,eggs);
+        contentValues.put(MILK,milk);
+        contentValues.put(HONEY,honey);
+        contentValues.put(GLUTEN,gluten);
+        contentValues.put(LUPIN,lupin);
+        contentValues.put(SESAME,sesame);
+        contentValues.put(ALGAE,algae);
+        contentValues.put(SHELLFISH,shellfish);
+        contentValues.put(SOY,soy);
+        contentValues.put(PEANUTS,peanuts);
+        contentValues.put(SULPHITE,sulphite);
+        contentValues.put(NUTS,nuts);
+        contentValues.put(MUSTARD,mustard);
+        contentValues.put(CELERY,celery);
+        contentValues.put(CORN,corn);
 
 
-       long result = db.insert(TABLE_NAME,null ,contentValues);
-       if(result == -1)
-           return false;
-       else
-           return true;
+        long result = db.insert(PROFILES_TABLE,null ,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
     }
 
     public boolean insertUIDToUsers(String userId) {
@@ -108,7 +107,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         if(res.getCount()==0)
             return false;
         else
-           return true;
+            return true;
     }
 
 }
