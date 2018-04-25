8232f7931943331a71f2a46f45acab827e0429c7
gaeaStiller <gaea.stiller@gmail.com>
2018-04-25 20:07:58 +0300

    saving user's input

--- app/src/main/java/com/example/mikhal/wiseatapp/SearchIngredients.java
+++ app/src/main/java/com/example/mikhal/wiseatapp/SearchIngredients.java
@@ -6,8 +6,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class SearchIngredients extends AppCompatActivity {
+    String ingredients;
+    Button btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +21,28 @@ public class SearchIngredients extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        btnSearch = (Button)findViewById(R.id.btnSearch);
 
+        search();
 
     }
+
+   public void search(){
+       btnSearch.setOnClickListener(
+               new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v){
+
+                       SearchView searchWord = (SearchView) findViewById(R.id.search);
+                       CharSequence query = searchWord.getQuery();
+                       ingredients=  query.toString();
+                        // here we can send the string- ingredients to the function that will transform the string to individual ingredients
+                   }
+               }
+
+
+       );
+
+   }
 
 }
